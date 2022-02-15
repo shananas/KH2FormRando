2439a2994a6f6599d4989153e24ddae7a70ad951
shananas <s.odea@comcast.net>
2022-02-15 12:59:06 -0500

    V2

--- Randomize.py
+++ Randomize.py
@@ -16,7 +16,7 @@ WI = [1623,1642,1644,2401,1646,1648,1650]
 XM = [2389,2390,2391,2399,2392,2393,2394]
 
 #Do randomization
-base = random.choice([0,2,3]) #One-Keyblade Forms
+base = random.choice([0,2,3,6]) #No Dual-Wield
 randomresult = [0,1,2,3,4,5,6]
 random.shuffle(randomresult)
 while randomresult[0] != base:
@@ -26,7 +26,12 @@ while randomresult[0] != base:
 for world in [ZZ,NM,TR,WI,XM]:
     for i in range(7):
         j = randomresult[i]
+        #Form[i] is turned into form[j]
         g[world[i]] = f[world[j]]
+        if i == 3: #Replaces Limit Form
+            mset = ['','_BTLF','_MAGF','_KH1F','_TRIF','_ULTF','_HTLF']
+            mset = 'P_EX100'+mset[j]+'_LIMIT.mset'
+            g[world[i]]['AnimationName'] = mset
 
 #Final fixes
 for i in g:
